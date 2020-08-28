a3fffcb48300c15d2334a12382f94dc3fe971ef9
Derpimort <jatinsaini580@gmail.com>
2020-08-28 23:53:05 +0530

    Add basic api funcs

---
+++ api.py
@@ -0,0 +1,17 @@
+from newsapi import NewsApiClient
+
+class Api(NewsApiClient):
+    def __init__(self, apikey):
+        if len(apikey.split(".")) > 1:
+            with open(apikey, "r") as apikeyf:
+                apikey = apikeyf.readline()
+
+        super().__init__(api_key=apikey)
+    
+    def get_topn(self, query):
+        top_headlines = super().get_top_headlines(q=query, category='business', language='en')
+
+        assert top_headlines['status']=='ok', "Error, fetching news from api"
+
+        for article in top_headlines['articles']:
+            yield article['title']
