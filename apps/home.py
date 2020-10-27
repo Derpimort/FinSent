432747bd654a1a5eff2ed9ca7afc79ab735c8629
derpimort <jatinsaini580@gmail.com>
2020-10-28 00:44:00 +0530

    centered buttons with dcc, works fine.

--- apps/home.py
+++ apps/home.py
@@ -1,28 +1,15 @@
 import os
 import dash
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 from app import app
 
-layout = dbc.Container( 
+layout = html.Div( 
     [
-        dbc.Row(
-            dbc.Col(
-                [
-                dbc.Button(dcc.Link("Daily report", href="/daily")),
-                html.P("--- OR ---"),
-                dbc.Button(dcc.Link("Individual stock analysis", href="/stonk"))
-                ],
-                width="auto",
-                align="center",
-                style={"height": "100%"},
-            ),
-            justify="center",
-            align="center",
-            className="h-75"
-    )
+    dcc.Link("Daily report", href="/daily"),
+    html.P("--- OR ---"),
+    dcc.Link("Individual stock analysis", href="/stonk")
     ],
-    style={"height":"100vh", "padding-top":"50px"}
+    style={"height":"100vh", "padding-top":"50px", "text-align":"center"}
 )
