e1983ccb65dc3b67f0d2b172b5aee5dcd8e8102e
derpimort <jatinsaini580@gmail.com>
2020-10-28 00:57:31 +0530

    Add css file locally, home button aesthetics.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import dash
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, 
+        #external_stylesheets=external_stylesheets, 
+        suppress_callback_exceptions=True)
 
 server = app.server
--- apps/home.py
+++ apps/home.py
@@ -7,9 +7,10 @@ from app import app
 
 layout = html.Div( 
     [
-    dcc.Link("Daily report", href="/daily"),
-    html.P("--- OR ---"),
-    dcc.Link("Individual stock analysis", href="/stonk")
+    dcc.Link("Daily report", href="/daily", className="button"),
+    html.P("--- OR ---", style={"margin":"20px"}),
+    dcc.Link("Individual stock analysis", href="/stonk", className="button")
     ],
-    style={"height":"100vh", "padding-top":"50px", "text-align":"center"}
+    style={"height":"100vh", "padding-top":"50px", "text-align":"center"},
+    className="container"
 )
