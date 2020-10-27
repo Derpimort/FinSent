d9d62a7c179b363947b81b9e68aa3545fcacb1f4
derpimort <jatinsaini580@gmail.com>
2020-10-28 00:41:12 +0530

    Try dbc, fuks it up.

--- apps/home.py
+++ apps/home.py
@@ -6,10 +6,23 @@ import dash_html_components as html
 
 from app import app
 
-layout = dbc.Container(
+layout = dbc.Container( 
     [
-        dbc.Button(dcc.Link("Daily report", href="/daily")),
-        html.P("--- OR ---"),
-        dbc.Button(dcc.Link("Individual stock analysis", href="/stonk"))
-    ]
+        dbc.Row(
+            dbc.Col(
+                [
+                dbc.Button(dcc.Link("Daily report", href="/daily")),
+                html.P("--- OR ---"),
+                dbc.Button(dcc.Link("Individual stock analysis", href="/stonk"))
+                ],
+                width="auto",
+                align="center",
+                style={"height": "100%"},
+            ),
+            justify="center",
+            align="center",
+            className="h-75"
+    )
+    ],
+    style={"height":"100vh", "padding-top":"50px"}
 )
