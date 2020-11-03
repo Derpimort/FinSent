6785e2a49b7cb802cd0c3300cb87d3d40d334d04
derpimort <jatinsaini580@gmail.com>
2020-11-04 00:30:11 +0530

    Footer looks good.

---
+++ static_elements.py
@@ -0,0 +1,39 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Epic guy -> https://codepen.io/scanfcode/pen/MEZPNd
+footer = html.Footer([
+            html.Div([
+                html.Div([
+                    html.H6("By Derpimort"),
+                    html.Ul([
+                        html.Li(
+                            html.A(
+                                html.I(className="fa fa-github"), 
+                                href="https://github.com/Derpimort",
+                                className="github"
+                            )
+                        ),
+                        html.Li(
+                            html.A(
+                                html.I(className="fa fa-linkedin"), 
+                                href="https://www.linkedin.com/in/derpimort/",
+                                className="linkedin"
+                            )
+                        )
+                    ], className="social-icons")
+                ], className="row center"),
+                html.Hr(),
+                html.Div([
+                    html.H6("FinBERT"),
+                    html.Ul([
+                        html.Li(
+                            html.A(
+                                html.I(className="fa fa-github"), 
+                                href="https://github.com/ProsusAI/finBERT",
+                                className="github"
+                            )
+                        )], className="social-icons")
+                ], className="row center")
+            ], className="container")
+        ], className="site-footer")
