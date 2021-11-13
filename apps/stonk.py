042a47554a2d3f0ad173b335f3f7bf2948d35548
Derpimort <jatinsaini580@gmail.com>
2021-11-13 23:27:13 +0530

    ADD: navbar with stock dropdown

--- apps/stonk.py
+++ apps/stonk.py
@@ -15,6 +15,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
+from finsent.data_helpers import StonkData
 
 from finsent.stock import Stock
 
@@ -23,9 +24,26 @@ from finsent.constants import DATA_DIR, STOCKS_DIR
 
 from app import app
 
+stonk_helper = StonkData(data_dir=DATA_DIR, stonks_dir=STOCKS_DIR)
 
 # Dashboard components layout
-layout = html.Div(children=[])
+layout = html.Div(children=[
+    html.Div([
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id='stonk-filter-stocks',
+                    options=stonk_helper.get_stonks_dict(),
+                    multi=False,
+                    placeholder="Select Stock"
+                )
+            ], className="six columns"),
+            html.Div([
+                html.Button("Show me Da Powaa!", id='stonk-filter-submit', n_clicks=0, className="submit-button"),
+            ], className="six columns")
+        ], className="row"),
+    ], className="navbar top-border"),
+])
 
 
 if __name__ == '__main__':
