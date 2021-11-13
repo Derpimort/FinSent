7ada9ed0e203fcf9ce2a74a4911d60abb116913e
Derpimort <jatinsaini580@gmail.com>
2021-11-14 00:31:44 +0530

    ADD: Basic stonk page ready

--- apps/stonk.py
+++ apps/stonk.py
@@ -7,6 +7,7 @@
 
 import os
 import dash
+from dash.exceptions import PreventUpdate
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
@@ -16,6 +17,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
 from finsent.data_helpers import StonkData
+from finsent.plot_helpers import StonkPlots
 
 from finsent.stock import Stock
 
@@ -25,6 +27,7 @@ from finsent.constants import DATA_DIR, STOCKS_DIR
 from app import app
 
 stonk_helper = StonkData(data_dir=DATA_DIR, stonks_dir=STOCKS_DIR)
+stonk_plot_helper = StonkPlots(*stonk_helper.get_df())
 
 # Dashboard components layout
 layout = html.Div(children=[
@@ -35,6 +38,7 @@ layout = html.Div(children=[
                     id='stonk-filter-stocks',
                     options=stonk_helper.get_stonks_dict(),
                     multi=False,
+                    value=stonk_helper.symbol,
                     placeholder="Select Stock"
                 )
             ], className="six columns"),
@@ -43,8 +47,50 @@ layout = html.Div(children=[
             ], className="six columns")
         ], className="row"),
     ], className="navbar top-border"),
+    html.Div([
+        html.Div([
+            html.H2("Stock Chart"),
+            dcc.Loading([
+                html.Div([
+                    dcc.Graph(id="stock-line-chart", 
+                    #figure=stonk_plot_helper.empty_plot(),
+                    figure=stonk_plot_helper.get_stock_chart(),
+                    )           
+                ])
+            ])
+        ], className="eight columns graph-section"),
+        html.Div([
+            html.H2("Sentiment Guage"),
+            dcc.Loading([
+                html.Div([
+                    dcc.Graph(id="sentiment-guage-chart", 
+                    #figure=stonk_plot_helper.empty_plot(),
+                    figure=stonk_plot_helper.get_sentiment_guage(),
+                    )           
+                ])
+            ])
+        ], className="four columns graph-section"),
+    ], className="row m-4 mt-16"),
 ])
 
+
+@app.callback(
+    [Output('stock-line-chart', 'figure'),
+    Output('sentiment-guage-chart', 'figure')],
+    [Input('stonk-filter-submit', 'n_clicks')],
+    [State('stonk-filter-stocks', 'value')])
+def update_charts(n_clicks, dropdown):
+    if n_clicks==0 or dropdown is None:
+        raise PreventUpdate("Invalid selection")
+
+    stonk_plot_helper.update_instance(
+        *stonk_helper.get_df(symbol=dropdown)
+    )
+
+    return (
+        stonk_plot_helper.get_stock_chart(), 
+        stonk_plot_helper.get_sentiment_guage()
+    )
 
 if __name__ == '__main__':
     app.layout = layout
